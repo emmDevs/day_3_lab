4c8cd4bb50e2dca40eade26e267ed7906c63dd3f
Emma Hack <ehack78@gmail.com>
2020-10-14 12:39:16 +0100

    day_3 labs

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,44 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(str_1, str_2):
+    return str_1 + str_2
+
+def add_string_as_number(str_1, str_2):
+    return int(str_1) + int(str_2) 
+
+months = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+def number_to_full_month_name(num):
+    return months[num - 1]
+
+month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+def number_to_short_month_name(num):
+    return month[num - 1]
+
+def volume_of_cube(length):
+    return length * 3
+
+def reverse_string(str):
+    string = [str]
+    new_string = []
+    for letter in word:
+        new_string.append(string.pop)
+    return new_string
+
+def fahrenheit_to_celsius(num):
+    return num  - 30 
